ed844ac6aaa23c89ca9adaaaa6ff4fe154a651cb
董聿凌 <gb2013429@gmail.com>
2022-02-17 00:18:39 +0800

    111.02.17

--- js/week3.js
+++ js/week3.js
@@ -33,7 +33,7 @@ const app = createApp({
             axios.get(url)
             .then((res)=>{
                 this.products =res.data.products;
-               // console.log(Object.values(this.products))//物件轉陣列
+               console.log(Object.values(this.products))//物件轉陣列
                 Object.values(this.products).forEach((item)=>{
                     console.log(item)
                 })
